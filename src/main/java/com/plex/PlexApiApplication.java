d7db7e21fea914184b10c99714abd4ee5f523969
Austin Huynh <huynhaus@gmail.com>
2020-04-08 00:05:17 -0400

    Initialze project with spring boot

---
+++ src/main/java/com/plex/PlexApiApplication.java
@@ -0,0 +1,13 @@
+package com.plex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plex/PlexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
